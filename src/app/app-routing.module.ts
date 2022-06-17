2846b63d565c2ee0efc2c4383259bf4a4a724938
cordeiro100 <mosscordeiro@hotmail.com>
2022-06-17 10:45:44 -0300

    first-step

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './views/cadastro/cadastro.component';
+import { HomeComponent } from './views/home/home.component';
+
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "cadastro", component: CadastroComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+
+import { CadastroComponent } from './views/cadastro/cadastro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+
+    CadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
